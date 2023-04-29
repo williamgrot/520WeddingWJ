8531d9dc9c81b27515f0b5688dce89bdc92433c5
williamgrot <williamgrot@gmail.com>
2023-04-29 00:39:22 -0500

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -93,25 +93,25 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "15px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "26px",
+                    "top": "24px",
                 });
                 $('header .navicon').css({
-                    "top": "34px",
+                    "top": "32px",
                 });
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "15px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "41px",
+                    "top": "24px",
                 });
                 $('header .navicon').css({
-                    "top": "48px",
+                    "top": "32px",
                 });
             }
         });
